89abbb3bfce944d180e4ca5c9e99dfa645e0b5dd
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-18 01:42:42 -0600

    Mobile patch

--- assets/site.js
+++ assets/site.js
@@ -2,7 +2,7 @@
    site.js — shared helpers for all pages
    ============================================================ */
    
-   export const _VERSION = "1.0.0b";
+   export const _VERSION = "1.0.0c";
    
 /**
  *
