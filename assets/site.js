49d68d853834b0d18f23771e5317249e6cc9ae15
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-18 06:14:14 -0600

    Fade wrap

--- assets/site.js
+++ assets/site.js
@@ -2,7 +2,7 @@
    site.js — shared helpers for all pages
    ============================================================ */
    
-   export const _VERSION = "1.0.0e";
+   export const _VERSION = "1.0.0.6";
    
 /**
  *
