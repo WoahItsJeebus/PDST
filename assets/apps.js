b43b3da1df44e8484e8b9f966db1cebb80f7eee9
WoahItsJeebus <147510817+WoahItsJeebus@users.noreply.github.com>
2026-01-17 18:26:42 -0600

    Setup 1.0.0

---
+++ assets/apps.js
@@ -0,0 +1,21 @@
+export const APPS = [
+	{
+		section: "Skate.",
+		key: "skateTimers",
+		title: "Reset Timers",
+		href: "https://woahitsjeebus.github.io/JSTC/",
+		icon: "https://shared.fastly.steamstatic.com/community_assets/images/apps/3354750/741cefdfeabf38b28288dab2cdbcf848995b85cb.ico",
+		// state: "ok" | "loading" | "error" | "off"
+		state: "ok",
+		// external: true, // open a new tab
+		// ariaLabel: "Open Timers",
+	},
+	{
+		section: "Roblox",
+		key: "Roblox_Friends_Since_checker",
+		title: "\"Friends Since\" Checker",
+		href: "https://woahitsjeebus.github.io/JFSC/",
+		icon: "https://raw.githubusercontent.com/WoahItsJeebus/JFSC/refs/heads/main/favicon.ico",
+		state: "ok",
+	},
+]
